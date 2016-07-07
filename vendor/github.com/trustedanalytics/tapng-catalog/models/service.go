41f451773e5e3b08f74ae4d470f4798961604f7e
Pawel Fierek <pawel.fierek@intel.com>
2016-07-07 17:35:25 +0200

    DPNG-9081 Added vendor to enable TC configuration

---
+++ vendor/github.com/trustedanalytics/tapng-catalog/models/service.go
@@ -0,0 +1,34 @@
+package models
+
+import "reflect"
+
+func init() {
+	RegisterType("Plans", reflect.TypeOf(ServicePlan{}))
+	RegisterType("Cost", reflect.TypeOf(ServicePlanCost{}))
+}
+
+type Service struct {
+	Id          string        `json:"id"`
+	Name        string        `json:"name"`
+	Description string        `json:"description"`
+	Bindable    bool          `json:"bindable"`
+	TemplateId  string        `json:"templateId"`
+	State       string        `json:"state"`
+	Plans       []ServicePlan `json:"plans"`
+	AuditTrail  AuditTrail    `json:"auditTrail"`
+}
+
+type ServicePlan struct {
+	Id          string          `json:"id"`
+	Name        string          `json:"name"`
+	Description string          `json:"description"`
+	Cost        ServicePlanCost `json:"cost"`
+	AuditTrail  AuditTrail      `json:"auditTrail"`
+}
+
+type ServicePlanCost struct {
+	Currency string `json:"currency"`
+	//TODO DPNG-8533 define other attributes of cost
+}
+
+//todo add State type
